070ba3cffb289dfb10000af0fc2697bfd74559a4
Socks <patricksutcliffe123@gmail.com>
2022-04-07 13:57:56 -0400

    created logic for checking user input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,30 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner answerObj = new Scanner(System.in);
         System.out.println("You are in a land full of dragons. in front of you,");
         System.out.println("you see two caves. In one cave, the dragon is friendly");
         System.out.println("and will share his treasure with you. THe other dragon");
         System.out.println("is hungry and will eat you on sight.");
-        System.out.println("Which cave will you go into? (1 or 2)");
+        String answer = null;
 
+        while ((answer!="1")||(answer!="2")) {
+
+            System.out.println("Which cave will you go into? (1 or 2)");
+            answer = answerObj.nextLine();
+            if (answer.equals("1")) {
+                System.out.println(answer);
+                break;
+            } else if (answer.equals("2")) {
+                System.out.println(answer);
+                break;
+            } else {
+                System.out.println("Please enter valid answer");
+            }
+        }
     }
 }
